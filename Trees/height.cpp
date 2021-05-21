24c8204e0e880dc939f18c88f644542b0bc3ad2f
deepdarshan21 <deepdarshan21@gmail.com>
2021-05-21 23:00:36 +0530

    Height of BST

---
+++ Trees/height.cpp
@@ -0,0 +1,60 @@
+// Program for finding height of tree
+
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *left, *right;
+};
+
+void insert(int data, Node **A)
+{
+    if (*A == NULL)
+    {
+        Node *temp;
+        temp = new Node;
+        temp->data = data;
+        temp->left = NULL;
+        temp->right = NULL;
+        *A = temp;
+    }
+    else if ((*A)->data >= data)
+        insert(data, &(*A)->left);
+    else
+        insert(data, &(*A)->right);
+}
+void print(Node *A)
+{
+    if (A != NULL)
+    {
+        print(A->left);
+        cout << A->data << "  ";
+        print(A->right);
+    }
+}
+int height(Node *A)
+{
+    if (A == NULL)
+        return -1;
+    // height(A->left);
+    // height(A->right);
+    return max(height(A->left), height(A->right)) + 1;
+}
+
+int main()
+{
+    Node *root = NULL;
+    insert(15, &root);
+    insert(10, &root);
+    insert(20, &root);
+    insert(8, &root);
+    insert(12, &root);
+    insert(17, &root);
+    insert(25, &root);
+    print(root);
+    cout << endl;
+    cout << height(root) << endl;
+    cout << endl;
+    return 0;
+}
